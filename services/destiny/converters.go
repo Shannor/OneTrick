f5809a9e96c9c48d96ade37b1b85cbfa51dfe89c
Shannor Trotty <shannor.a.trotty@gmail.com>
2026-08-30 21:53:26 -0400

    fix(destiny): add missing Iron Banner mode strings to gameModeToActivityModes

--- services/destiny/converters.go
+++ services/destiny/converters.go
@@ -583,14 +583,33 @@ func gameModeToActivityModes(gameMode api.GameMode) ([]string, error) {
 	case api.GameModeAll:
 		return nil, nil
 	case api.GameModeCompetitive:
-		return []string{"Competitive: Matchmade", "Competitive"}, nil
+		return []string{
+			"Competitive: Matchmade",
+			"Competitive",
+			"Survival",
+			"Control Competitive",
+			"Clash Competitive",
+		}, nil
 	case api.GameModeQuickPlay:
-		return []string{"Control Quickplay", "Control", "Control: Matchmade", "Clash", "Clash Quickplay"}, nil
+		return []string{
+			"Control Quickplay",
+			"Control",
+			"Zone Control",
+			"Control: Matchmade",
+			"Clash",
+			"Clash Quickplay",
+		}, nil
 	case api.GameModeIronBanner:
 		return []string{
+			"Iron Banner",
+			"Iron Banner Control",
+			"Iron Banner Zone Control",
+			"Iron Banner Clash",
+			"Iron Banner Supremacy",
+			"Iron Banner Rift",
+			"Iron Banner Tribute / Supremacy",
 			"Iron Banner Zone:Control",
 			"Iron Banner:Control",
-			"Iron Banner",
 			"Iron Banner:Supremacy",
 			"Iron Banner:Rift",
 			"Iron Banner:Clash",
@@ -602,7 +621,12 @@ func gameModeToActivityModes(gameMode api.GameMode) ([]string, error) {
 			"Iron Banner: Eruption",
 		}, nil
 	case api.GameModeTrials:
-		return []string{"Trials of Osiris", "Trials of Osiris: Matchmade"}, nil
+		return []string{
+			"Trials of Osiris",
+			"Trials of Osiris: Matchmade",
+			"Trials Survival",
+			"Trials Countdown",
+		}, nil
 	default:
 		return nil, nil
 	}
